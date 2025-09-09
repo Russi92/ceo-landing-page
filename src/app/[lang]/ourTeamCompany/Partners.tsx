6fe15e31cd395955540fa37d3c1bc8603d79a81e
Eng.Russi <m.hassan@sharidah.sa>
2025-09-09 15:49:20 +0300

    responsive team card

--- src/app/[lang]/ourTeamCompany/Partners.tsx
+++ src/app/[lang]/ourTeamCompany/Partners.tsx
@@ -233,6 +233,150 @@
 //     )
 // }
 
+// "use client";
+
+// import Image from "next/image";
+// import React, { useRef } from "react";
+// import { useParams } from "next/navigation";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { Navigation, Mousewheel, Autoplay } from "swiper/modules";
+// import type { Swiper as SwiperType } from "swiper";
+// import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
+// // استيراد ملفات الـ CSS الخاصة بـ Swiper
+// import "swiper/css";
+// import "swiper/css/navigation";
+// import "swiper/css/autoplay"; // يفضل استيرادها
+
+// export default function Partners({
+//   dict,
+//   title,
+// }: {
+//   dict: any;
+//   title: string;
+// }) {
+//   const { lang } = useParams() as { lang?: string };
+//   const isArabic = lang === "ar";
+//   const swiperRef = useRef<SwiperType | null>(null);
+
+//   const partners = [
+//     {
+//       name: "Saudi3",
+//       url: "/images/team/russi.jpeg",
+//       title: "Mohamed hassan",
+//       position: "FrontEnd Developer",
+//     },
+//     {
+//       name: "Saudi3",
+//       url: "/images/team/darsh.jpeg",
+//       title: "Mustafa Gamel",
+//       position: "BackEnd Developer",
+//     },
+//     {
+//       name: "Saudi3",
+//       url: "/images/team/abdelrahman.png",
+//       title: "Abdulrahman Ahmed",
+//       position: "Full Stack Developer",
+//     },
+//     {
+//       name: "Saudi3",
+//       url: "/images/c.png",
+//       title: "Ahmed Ismael",
+//       position: "Full Stack Developer",
+//     },
+//     {
+//       name: "Saudi3",
+//       url: "/images/team/ahmed.jpeg",
+//       title: "Ahmed Mohamed",
+//       position: "BackEnd Developer",
+//     },
+//     {
+//       name: "Saudi3",
+//       url: "/images/c.png",
+//       title: "Mohamed Ayman",
+//       position: "FrontEnd Developer",
+//     },
+//   ];
+
+//   return (
+//     <section className="mb-10 " dir="ltr" id="team">
+//       <h3 className="text-center text-5xl text-primary mb-10">{title}</h3>
+
+//       <div className="relative w-full max-w-7xl mx-auto px-10">
+//         <Swiper
+//           modules={[Navigation, Mousewheel, Autoplay]}
+//           onSwiper={(swiper) => (swiperRef.current = swiper)}
+//           loop={true}
+//           spaceBetween={40}
+//           slidesPerView={1}
+//           grabCursor={true}
+//           centeredSlides={true} // ✅ الكروت تبقى في النص
+//           autoplay={{
+//             delay: 2000,
+//             disableOnInteraction: false,
+//             pauseOnMouseEnter: true,
+//             reverseDirection: true,
+//           }}
+//           breakpoints={{
+//             640: { slidesPerView: 2, spaceBetween: 20, centeredSlides: true },
+//             768: { slidesPerView: 3, spaceBetween: 20, centeredSlides: true },
+//             1024: { slidesPerView: 4, spaceBetween: 40, centeredSlides: false }, // ممكن تخليها false عشان الكبير يكون عادي
+//           }}
+//           className="w-full"
+//         >
+//           {partners.map((p, index) => (
+//             <SwiperSlide key={index}>
+//               <div
+//                 className="flex flex-col items-center p-6 rounded-2xl shadow-md bg-[#f1daa5]
+//                                             w-48 sm:w-60 sm:h-72 flex-shrink-0
+//                                             transition-all duration-300 ease-in-out transform hover:-translate-y-2 hover:scale-105 hover:shadow-xl"
+//               >
+//                 <div
+//                   className="relative w-28 h-28 sm:w-32 sm:h-32 rounded-full overflow-hidden 
+//                                                 flex items-center justify-center border-4 border-white shadow-sm mb-4"
+//                 >
+//                   <Image
+//                     src={p.url}
+//                     alt={p.name}
+//                     fill
+//                     className="object-cover"
+//                   />
+//                 </div>
+//                 <h4 className="text-gray-900 text-sm sm:text-sm text-center mb-1">
+//                   {p.title}
+//                 </h4>
+//                 <p className="text-gray-600 text-sm sm:text-base text-center">
+//                   {p.position}
+//                 </p>
+//               </div>
+//             </SwiperSlide>
+//           ))}
+//         </Swiper>
+
+//         {/* أزرار التنقل خارج حاوية الـ Swiper */}
+//         {/* أزرار التنقل خارج حاوية الـ Swiper */}
+//         <button
+//           onClick={() => swiperRef.current?.slidePrev()}
+//           className={`absolute top-1/2 transform -translate-y-1/2 z-10 w-10 h-10 flex items-center justify-center transition cursor-pointer font-bold
+//           left-2 sm:left-4 md:-left-12
+//           bg-[#f1daa5] text-black rounded-full shadow-md`}
+//         >
+//           <FaChevronLeft />
+//         </button>
+
+//         <button
+//           onClick={() => swiperRef.current?.slideNext()}
+//           className={`absolute top-1/2 transform -translate-y-1/2 z-10 w-10 h-10 flex items-center justify-center transition cursor-pointer font-bold
+//           right-2 sm:right-4 md:-right-12
+//           bg-[#f1daa5] text-black rounded-full shadow-md`}
+//         >
+//           <FaChevronRight />
+//         </button>
+//       </div>
+//     </section>
+//   );
+// }
+
 "use client";
 
 import Image from "next/image";
@@ -246,7 +390,7 @@ import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 // استيراد ملفات الـ CSS الخاصة بـ Swiper
 import "swiper/css";
 import "swiper/css/navigation";
-import "swiper/css/autoplay"; // يفضل استيرادها
+import "swiper/css/autoplay";
 
 export default function Partners({
   dict,
@@ -299,7 +443,7 @@ export default function Partners({
   ];
 
   return (
-    <section className="mb-10 " dir="ltr" id="team">
+    <section className="mb-10" dir="ltr" id="team">
       <h3 className="text-center text-5xl text-primary mb-10">{title}</h3>
 
       <div className="relative w-full max-w-7xl mx-auto px-10">
@@ -310,7 +454,7 @@ export default function Partners({
           spaceBetween={40}
           slidesPerView={1}
           grabCursor={true}
-          centeredSlides={true} // ✅ الكروت تبقى في النص
+          centeredSlides={true}
           autoplay={{
             delay: 2000,
             disableOnInteraction: false,
@@ -320,7 +464,7 @@ export default function Partners({
           breakpoints={{
             640: { slidesPerView: 2, spaceBetween: 20, centeredSlides: true },
             768: { slidesPerView: 3, spaceBetween: 20, centeredSlides: true },
-            1024: { slidesPerView: 4, spaceBetween: 40, centeredSlides: false }, // ممكن تخليها false عشان الكبير يكون عادي
+            1024: { slidesPerView: 4, spaceBetween: 40, centeredSlides: false },
           }}
           className="w-full"
         >
@@ -328,7 +472,7 @@ export default function Partners({
             <SwiperSlide key={index}>
               <div
                 className="flex flex-col items-center p-6 rounded-2xl shadow-md bg-[#f1daa5]
-                                            w-48 sm:w-60 sm:h-72 flex-shrink-0
+                                            w-full max-w-xs mx-auto h-72
                                             transition-all duration-300 ease-in-out transform hover:-translate-y-2 hover:scale-105 hover:shadow-xl"
               >
                 <div
@@ -353,8 +497,6 @@ export default function Partners({
           ))}
         </Swiper>
 
-        {/* أزرار التنقل خارج حاوية الـ Swiper */}
-        {/* أزرار التنقل خارج حاوية الـ Swiper */}
         <button
           onClick={() => swiperRef.current?.slidePrev()}
           className={`absolute top-1/2 transform -translate-y-1/2 z-10 w-10 h-10 flex items-center justify-center transition cursor-pointer font-bold
